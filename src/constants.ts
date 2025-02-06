3f9e2c100804f014ef51f387535350ada9ad78a8
Angel Lacret <alacret@gmail.com>
2025-02-06 16:49:11 -0500

    some other fixes

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,8 @@
 export const GITHUB_GRAPHQL_API = "https://api.github.com/graphql";
 export const DAYS_IN_INTERVAL = 5;
 export const BLACKLISTED_CODE_USERS = new Set<string>([
-    "altaciosthedev", "waqas", "fullstack900", "ghost", "dependabot[bot]", "unknown", "nicolas-toledo", "anjelysleal",
-    "juansebasmarin", "yamilachan", "kaikrmen", "metalmagno", "aovallegalan", "shedeed1", "yamilachan", "snyk-bot",
-    "amlingad", "brennenhamilton", "copilot-pull-request-reviewer"]);
+    "waqas", "fullstack900", "ghost", "dependabot[bot]", "unknown", "kaikrmen", "metalmagno", "aovallegalan",
+    "shedeed1","snyk-bot", "amlingad", "brennenhamilton", "copilot-pull-request-reviewer"]);
 export const AUTHOR_ALIAS_MAP = new Map<string, string>([
     ["yeferson hidalgo", "memimint"],
     ["jonathan miles", "jonmiles"],
